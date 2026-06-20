d6e592933e9e9529085f96e244d255af0caaeeb0
Vishal Magesh <66678881+mvishal08@users.noreply.github.com>
2026-06-20 15:39:06 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int>mp;
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+        {
+            int rem = target - nums[i];
+            if(mp.find(rem) != mp.end())
+            {
+                return {mp[rem], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+
+    }
+};
